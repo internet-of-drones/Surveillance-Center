6ff448485197f7c87f604a92c2d4bc9d4978db6e
Hào Nghiêm <qtvhao@gmail.com>
2022-08-13 23:59:24 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,3 +3,4 @@ git clone https://github.com/stella-cv/stella_vslam.git
 cd stella_vslam
 git submodule update -i --recursive
 docker build -t stella_vslam-desktop -f Dockerfile.desktop .
+docker build .
