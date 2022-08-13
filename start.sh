614f2bf48b962ec64f785ae04881dcf514352dcb
haonx <qtvhao@gmail.com>
2022-08-13 10:17:18 -0700

    s

--- start.sh
+++ start.sh
@@ -5,3 +5,5 @@ git submodule update -i --recursive
 docker build -t stella_vslam-desktop -f Dockerfile.desktop .
 cd ~/Surveillance-Center && \
 docker build -t surveillancecenter .
+DISPLAY=$(docker inspect cy-open | jq -r ".[0].NetworkSettings.Networks.bridge.IPAddress")":0.0"
+docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix:ro surveillancecenter
