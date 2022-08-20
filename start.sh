d303361a04c0f14d6bf05bdde0dacfcd54ee941d
haonx <qtvhao@gmail.com>
2022-08-20 11:45:13 -0700

    s

--- start.sh
+++ start.sh
@@ -7,9 +7,6 @@ cd ~/Surveillance-Center && \
 docker build -t surveillancecenter .
 DISPLAY=$(docker inspect cy-open | jq -r ".[0].NetworkSettings.Networks.bridge.IPAddress")":0.0"
 export MAP_ID="12"
-# shellcheck disable=SC2046
-rm $(find /ip-cam/ -type f -name "capture-$MAP_ID-*")
-(mkdir /ip-cam/ || true) && cd /ip-cam/ && openRTSP -D 1 -B 10000000 -b 10000000 -q -Q -F capture-$MAP_ID- -d 60 -P 60 -t -u admin a12345678 rtsp://192.168.68.242:554/onvif1
 record=$(find /ip-cam/ -type f -name "capture-$MAP_ID-*")
 # --entrypoint=bash \
 docker run -it --rm \
