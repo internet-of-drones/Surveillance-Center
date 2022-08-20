aa2286e45baff2830afdc793015271a92fb99a0f
haonx <qtvhao@gmail.com>
2022-08-20 11:47:57 -0700

    s

--- update-map.sh
+++ update-map.sh
@@ -1,6 +1,6 @@
 # shellcheck disable=SC2046
 rm $(find /ip-cam/ -type f -name "capture-$MAP_ID-*")
-cd /ip-cam/ && openRTSP -D 1 -B 10000000 -b 10000000 -q -Q -F "capture-$MAP_ID-" -d 60 -P 60 -t -u admin a12345678 rtsp://192.168.68.242:554/onvif1
+(cd /ip-cam/ && openRTSP -D 1 -B 10000000 -b 10000000 -q -Q -F "capture-$MAP_ID-" -d 60 -P 60 -t -u admin a12345678 rtsp://192.168.68.242:554/onvif1)
 
 ./run_video_slam --auto-term -v ./orb_vocab.fbow \
  -m ${MOVIE_PATH} -c ../example/aist/equirectangular.yaml \
